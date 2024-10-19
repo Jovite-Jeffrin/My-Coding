b5a6184a98d821df76c2b4fc0311ef2d4102ab78
Jovite Jeffrin A <122461797+Jovite-Jeffrin@users.noreply.github.com>
2024-10-19 11:17:45 +0530

    Create Recyclable and Low Fat Products.py

---
+++ Python/Leetcode/Select/Recyclable and Low Fat Products.py
@@ -0,0 +1,11 @@
+# Write a solution to find the ids of products that are both low fat and recyclable.
+
+# Return the result table in any order.
+
+# The result format is in the following example.
+
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return df[['product_id']]
