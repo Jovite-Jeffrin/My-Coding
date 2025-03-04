4e053698a5aab67b3cd7f1ad98e6ce1bbfb1e455
Jovite Jeffrin A <122461797+Jovite-Jeffrin@users.noreply.github.com>
2025-03-04 22:12:55 +0530

    Delete Python/Analyst Builder directory

--- Python/Analyst Builder/Food Divides Us.py
+++
@@ -1,9 +0,0 @@
-# In the United States, fast food is the cornerstone of it's very society. Without it, it would cease to exist.
-# But which region spends the most money on fast food?
-# Write a query to determine which region spends the most amount of money on fast food.
-
-# access datasets as pandas dataframes
-import pandas as pd;
-
-df = food_regions.groupby('region')['fast_food_millions'].sum().reset_index()
-df[df['fast_food_millions'] == df['fast_food_millions'].max()][['region']]
--- Python/Analyst Builder/readme.txt
+++
@@ -1,4 +0,0 @@
-Analyst Builder my Python Solutions
-
-Website: https://www.analystbuilder.com/
-Profile link: https://www.analystbuilder.com/u/user-ABh5MBwnH5oYbKnvAg-Ti
